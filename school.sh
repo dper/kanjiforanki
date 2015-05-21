8053527fe402270cf721251129dba77ffc79c517
Douglas Perkins <webmaster@dperkins.org>
2015-05-21 19:31:01 +0900

    Finished script.

--- school.sh
+++ school.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # This script generates lists for elementary and junior high school kanji study.
+# If you intend to make decks that correspond to Japanese elementary school and JHS
+# grades, this script is what you should use first.  After the resultant files are
+# created in decks/, that data can be imported into Anki.
 
 for list in 'elementary.1.txt' 'elementary.2.txt' 'elementary.3.txt' 'elementary.4.txt' 'elementary.5.txt' 'elementary.6.txt' 'elementary.txt' 'jhs.txt'
 do
