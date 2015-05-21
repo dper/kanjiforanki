c815b9b0e7b359096e756f0c4d1233391e859aa8
Douglas Perkins <webmaster@dperkins.org>
2015-05-21 19:29:57 +0900

    Finished script.

--- school.sh
+++ school.sh
@@ -4,7 +4,9 @@
 
 for list in 'elementary.1.txt' 'elementary.2.txt' 'elementary.3.txt' 'elementary.4.txt' 'elementary.5.txt' 'elementary.6.txt' 'elementary.txt' 'jhs.txt'
 do
+	echo '------------------------------------------'
+	echo "Generating deck for ${list}..."
 	cp lists/$list targetkanji.txt
-	./kanjiforanki.rb
+	nice ./kanjiforanki.rb
 	cp anki.txt decks/$list
 done
